f9c381fb62e26372b78670f73bd2c601848a3c52
anoriqq <shota.yoshikawa@anoriqq.com>
2020-05-12 23:28:42 +0900

    chore: add fake timers

--- __tests__/page.index.test.tsx
+++ __tests__/page.index.test.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Index from '../pages/index';
 
+jest.useFakeTimers();
+
 it('renders homepage unchanged', () => {
   const tree = renderer.create(<Index />).toJSON();
   expect(tree).toMatchSnapshot();
