25127513c4b1d0c4b58a22f9e111e6df11e81509
anoriqq <shota.yoshikawa@anoriqq.com>
2020-05-19 21:53:27 +0900

    chore: setup ui framework

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Provider } from 'reakit';
-import * as system from 'reakit-system-bootstrap';
+import { Global, css } from '@emotion/core';
+import styled from '@emotion/styled';
+import 'typeface-roboto';
+
+const globalStyles = css`
+  html,
+  body {
+    width: 100%;
+    margin: 0;
+  }
+`;
+
+const Container = styled.div({
+  margin: '2rem auto',
+  maxWidth: '700px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
 
 const Layout: React.FC = ({ children }) => (
   <>
-    <Provider unstable_system={system}>{children}</Provider>
+    <Global styles={globalStyles} />
+    <Container>{children}</Container>
   </>
 );
 
