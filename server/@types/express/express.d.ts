3bcd6b670374b64c1b2515da233e68bace455ca6
anoriqq <shota.yoshikawa@anoriqq.com>
2020-05-26 23:32:00 +0900

    chore: declare types

--- server/@types/express/express.d.ts
+++ server/@types/express/express.d.ts
@@ -1,10 +1,85 @@
 import { RequestHandler } from "express";
 import { ParsedQs } from "qs";
 declare module "express-serve-static-core" {
-  type Error = {
+  type APIError = {
     code: number;
     message: string;
   };
+  type Project = {
+    createTime?: string;
+    labels?: any;
+    lifecycleState?: string;
+    name?: string;
+    projectId?: string;
+    projectNumber?: string;
+  };
+  type Instance = {
+    cpuPlatform?: string;
+    creationTimestamp?: string;
+    description?: string;
+    disks?: {
+      autoDelete?: boolean;
+      boot?: boolean;
+      deviceName?: string;
+      diskSizeGb?: string;
+      interface?: string;
+      kind?: string;
+      mode?: string;
+      source?: string;
+      type?: string;
+    }[];
+    hostname?: string;
+    id?: string;
+    kind?: string;
+    labels?: any;
+    machineType?: string;
+    metadata?: {
+      items?: {
+        key?: string;
+        value?: string;
+      }[];
+    };
+    name?: string;
+    networkInterfaces?: {
+      name?: string;
+      network?: string;
+      networkIP?: string;
+      subnetwork?: string;
+      accessConfigs?: {
+        name?: string;
+        natIP?: string;
+        publicPtrDomainName?: string;
+      }[];
+    }[];
+    status?: string;
+    statusMessage?: string;
+    zone?: string;
+  };
+  type UserInfo = {
+    displayName?: string;
+    projectLifecycleState?: string;
+    instanceId?: string;
+  };
+  type MachineProfile = {
+    region: string;
+    zone: string;
+    machineType: string;
+    imageProject: string;
+    imageFamily: string;
+    diskSizeGb: string;
+    sshPublicKey?: string;
+  };
+  type Zones = {
+    name: string;
+    region: string;
+  }[];
+  type MachineTypes = {
+    name: string;
+  }[];
+  type Images = {
+    project: string;
+    family: string;
+  }[];
   type _ResBody<
     M extends
       | "all"
@@ -106,7 +181,7 @@ declare module "express-serve-static-core" {
     ): T;
     <
       P extends Params = ParamsDictionary,
-      ResBody = _ResBody<Method, null, any, any, null, any>,
+      ResBody = _ResBody<Method, UserInfo, any, any, any, any>,
       ReqBody = _ReqBody<Method, null, any, any, null, any>,
       ReqQuery = _ReqQuery<Method, null, ParsedQs, ParsedQs, null, ParsedQs>
     >(
@@ -115,7 +190,7 @@ declare module "express-serve-static-core" {
     ): T;
     <
       P extends Params = ParamsDictionary,
-      ResBody = _ResBody<Method, null, any, any, any, any>,
+      ResBody = _ResBody<Method, any, any, any, any, any>,
       ReqBody = _ReqBody<Method, null, any, any, any, any>,
       ReqQuery = _ReqQuery<Method, null, ParsedQs, ParsedQs, ParsedQs, ParsedQs>
     >(
@@ -124,7 +199,7 @@ declare module "express-serve-static-core" {
     ): T;
     <
       P extends Params = ParamsDictionary,
-      ResBody = _ResBody<Method, null, any, any, any, any>,
+      ResBody = _ResBody<Method, any, any, any, any, any>,
       ReqBody = _ReqBody<Method, null, any, any, any, any>,
       ReqQuery = _ReqQuery<Method, null, ParsedQs, ParsedQs, ParsedQs, ParsedQs>
     >(
@@ -133,7 +208,7 @@ declare module "express-serve-static-core" {
     ): T;
     <
       P extends Params = ParamsDictionary,
-      ResBody = _ResBody<Method, null, any, any, any, any>,
+      ResBody = _ResBody<Method, any, any, any, any, any>,
       ReqBody = _ReqBody<Method, null, any, any, any, any>,
       ReqQuery = _ReqQuery<Method, null, ParsedQs, ParsedQs, ParsedQs, ParsedQs>
     >(
@@ -142,54 +217,56 @@ declare module "express-serve-static-core" {
     ): T;
     <
       P extends Params = ParamsDictionary,
-      ResBody = _ResBody<
-        Method,
-        {
-          vm: string;
-        },
-        null,
-        any,
-        any,
-        any
-      >,
-      ReqBody = _ReqBody<
-        Method,
-        null,
-        {
-          region: string;
-          zone: string;
-          machineType: string;
-          imageProject: string;
-          imageFamily: string;
-          diskSizeGb: string;
-          sshPublicKey: string;
-        },
-        any,
-        any,
-        any
-      >,
-      ReqQuery = _ReqQuery<Method, null, null, ParsedQs, ParsedQs, ParsedQs>
+      ResBody = _ResBody<Method, Project, Project, any, any, any>,
+      ReqBody = _ReqBody<Method, null, null, any, null, any>,
+      ReqQuery = _ReqQuery<Method, null, null, ParsedQs, null, ParsedQs>
     >(
-      path: "/vm",
+      path: "/project",
       ...handlers: Array<RequestHandler<P, ResBody, ReqBody, ReqQuery>>
     ): T;
     <
       P extends Params = ParamsDictionary,
-      ResBody = _ResBody<
-        Method,
-        {
-          name?: string;
-          region?: string;
-        }[],
-        any,
-        any,
-        any,
-        any
-      >,
+      ResBody = _ResBody<Method, Instance, Instance, any, any, any>,
+      ReqBody = _ReqBody<Method, null, MachineProfile, any, null, any>,
+      ReqQuery = _ReqQuery<Method, null, null, ParsedQs, null, ParsedQs>
+    >(
+      path: "/instance",
+      ...handlers: Array<RequestHandler<P, ResBody, ReqBody, ReqQuery>>
+    ): T;
+    <
+      P extends Params = ParamsDictionary,
+      ResBody = _ResBody<Method, Zones, any, any, any, any>,
       ReqBody = _ReqBody<Method, null, any, any, any, any>,
       ReqQuery = _ReqQuery<Method, null, ParsedQs, ParsedQs, ParsedQs, ParsedQs>
     >(
-      path: "/vm/zones",
+      path: "/instance/zones",
+      ...handlers: Array<RequestHandler<P, ResBody, ReqBody, ReqQuery>>
+    ): T;
+    <
+      P extends Params = ParamsDictionary,
+      ResBody = _ResBody<Method, MachineTypes, any, any, any, any>,
+      ReqBody = _ReqBody<Method, null, any, any, any, any>,
+      ReqQuery = _ReqQuery<
+        Method,
+        {
+          zone: string;
+        },
+        ParsedQs,
+        ParsedQs,
+        ParsedQs,
+        ParsedQs
+      >
+    >(
+      path: "/instance/machineTypes",
+      ...handlers: Array<RequestHandler<P, ResBody, ReqBody, ReqQuery>>
+    ): T;
+    <
+      P extends Params = ParamsDictionary,
+      ResBody = _ResBody<Method, Images, any, any, any, any>,
+      ReqBody = _ReqBody<Method, null, any, any, any, any>,
+      ReqQuery = _ReqQuery<Method, null, ParsedQs, ParsedQs, ParsedQs, ParsedQs>
+    >(
+      path: "/instance/images",
       ...handlers: Array<RequestHandler<P, ResBody, ReqBody, ReqQuery>>
     ): T;
   }
