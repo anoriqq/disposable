3cf9a7b6b73206b53c8e077a071ea04a1ae6b8cd
anoriqq <shota.yoshikawa@anoriqq.com>
2020-06-07 09:45:43 +0900

    chore: add english ver

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -6,11 +6,23 @@ import Layout from 'components/layout';
 const TermsPage: React.FC = () => (
   <Layout>
     <div>
+      <h2>プライバシーポリシー</h2>
       <div>
-        disposable.anoriqq.comが取得/永続化したデータはサービス運営/向上の目的のみに使用します。
+        disposable.anoriqq.comが取得/永続化したデータはサービス運営と向上の目的のみに使用します。
       </div>
       <div>個人情報を第三者に提供しません。</div>
       <div>個人情報保護に関する法令を遵守します。</div>
+      <h2>Privacy Policy</h2>
+      <div>
+        The data obtained / persistent by disposable.anoriqq.com will be used
+        for service operation and It is used only for the purpose of
+        improvement.
+      </div>
+      <div>We will not provide personal information to any third party.</div>
+      <div>
+        We will comply with laws and regulations regarding the protection of
+        personal information.
+      </div>
     </div>
     <Link href="/">
       <a>HOME</a>
