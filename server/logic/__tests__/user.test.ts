5a304c9663fd3f787b8ea364f6aea5e45b1a55d1
anoriqq <shota.yoshikawa@anoriqq.com>
2020-06-07 15:42:03 +0900

    test: add user db test

---
+++ server/logic/__tests__/user.test.ts
@@ -0,0 +1,37 @@
+import mongoUnit from 'mongo-unit';
+import mongoose from 'mongoose';
+
+import { setupMongoose, User } from '../../lib/db';
+import { deleteUser } from '../user';
+
+describe('User', () => {
+  const UserData = {
+    users: [
+      {
+        _id: 1234,
+        displayName: 'Test User',
+        accessToken: 'access_token',
+      },
+    ],
+  };
+
+  beforeAll(async () => {
+    process.env.MONGODB_CONNECTION_STRING = await mongoUnit.start();
+    setupMongoose();
+    mongoUnit.load(UserData);
+  });
+
+  afterAll(async () => {
+    mongoUnit.drop();
+    mongoUnit.stop();
+    mongoose.disconnect();
+  });
+
+  it('Delete user', async () => {
+    deleteUser({ userId: 1234 });
+
+    const user = await User.findById(1234).exec();
+
+    expect(user).toBeNull();
+  });
+});
